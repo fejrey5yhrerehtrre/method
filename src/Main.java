2aa79be24cd0ec018e6938ed3fb82d75caa83abb
fejrey5yhrerehtrre <ukalmamatov60@gmail.com>
2024-11-21 16:47:37 +0600

    first try

---
+++ src/Main.java
@@ -0,0 +1,113 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+//        for (int i = array.length - 1; i >= 0; i--) {
+//            System.out.println(array[i]);
+//
+//
+//        }
+//    }
+//
+//    public static int[] exampl(int[] a) {
+//        for (int i = a.length - 1; i >= 0; i--) {
+//            System.out.println(a[i]);
+//
+//        }
+//        return array;
+//    }
+
+
+//            int[] array = {1,2,3,4,5,6,7,8};
+//            System.out.println("normal array:"+ Arrays.toString(array));
+//            System.out.println("reverse array:"+Arrays.toString(example(array)));
+//        }
+//        public static int[] example(int[] a){
+//            int[] s = new int[a.length];
+//            int count = 0;
+//            for (int i = a.length - 1; i >= 0; i--) {
+//                s[count] = a[i];
+//                count++;
+//            }
+//            return s;
+//
+//        int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+//        System.out.print("array:");
+//        for (int a : array) {
+//            System.out.print(a + " ");
+//        }
+//        System.out.println();
+////        System.out println ("sum:" + getSum(array));
+//    }
+//            public static int getSum(int[] array) {
+//                int sum = 0;
+//                for (int i = 0; 1 < array.length; i++){
+//                    sum += array[i];
+//
+//            }
+//            return sum;
+//
+//    }}
+
+
+//        int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+//        System.out.print("array: ");
+//        for (int a : array) {
+//            System.out.print(a + " ");
+//        }
+//        System.out.println();
+//        System.out.println("sum: " + sum(array));
+//    }
+//
+//    public static int sum(int[] array) {
+//
+//        int sum = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            sum += array[i];
+//        }
+
+
+//        return sum;
+//    }}
+
+//Scanner scanner = new Scanner(System.in);
+//int a = scanner.nextInt();
+//zvezda(a);
+//    }
+//public static void zvezda(int count) {
+//    for (int i = 0; i < count; i++) {
+//        System.out.print("*");
+//    }}}
+//int[] array = {4, 7, 2};
+//        System.out.println(Arrays.toString(array));
+//        System.out.println(Arrays.toString(example(array)));
+//        }
+//
+//public static int[] example(int[] a) {
+//    int[] s = new int[a.length];
+//    int count = 0;
+//    for (int i = a.length - 1; i >= 0; i--) {
+//        s[count] = a[i];
+//        count++;
+//    }
+//    return s;
+
+
+
+
+
+//    int[] array = {4, 7, 2};
+//    System.out.println(Arrays.toString(array));
+//    System.out.println(Arrays.toString(example(array)));
+//}
+//
+//public static int[] example(int[] a) {
+//    int[] s = new int[a.length];
+//    int count = 0;
+//    for (int i = a.length - 1; i >= 0; i--) {
+//        s[count] = a[i];
+//        count++;
+//    }
+//    return s;
+//    }}}}
