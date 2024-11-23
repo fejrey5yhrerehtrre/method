57a0f465b5ffbfc287657b9220169e85e9e036e7
fejrey5yhrerehtrre <ukalmamatov60@gmail.com>
2024-11-23 16:42:48 +0600

    qwe

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
         for (int i = array.length - 1; i >= 0; i--) {
             System.out.println(array[i]);//dfdfdfdf
-
+//wert
 //
 //        }
 //    }
